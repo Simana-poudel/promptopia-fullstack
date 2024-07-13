a4b086b4d52e00523c2b7fc2ecde173acda65454
Simana Poudel <83345496+Simana-poudel@users.noreply.github.com>
2024-07-13 09:31:06 +0545

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,12 +25,12 @@ const Nav = () => {
         flex-center'>
             <Image 
             src='/assets/images/logo.svg' 
-            alt='Promptopia Logo'
+            alt='IdeaPalette Logo'
             width={30}
             height={30}
             className='object-contain'
             />
-            <p className='logo_text'>Promptopia</p>
+            <p className='logo_text'>IdeaPalette</p>
         </Link>
 
 
